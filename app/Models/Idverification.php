a6d92cc00e268e597592d77e6ddb892766f90f0c
charles ono <investorshi20@gmail.com>
2025-07-27 12:46:00 -1200

    model issue

--- app/Models/Idverification.php
+++ app/Models/Idverification.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User; // Import User model
 class IdVerification extends Model
 {
     use HasFactory;
+    
+    protected $guarded = [];
+
+    protected $table = 'id_verifications';
+
+    // This defines the relation to User model
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
